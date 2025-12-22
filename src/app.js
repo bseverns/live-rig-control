92a27ab23dd32e493867064eb11bc3157b874af0
Ben Severns <severns3@gmail.com>
2025-12-22 12:53:49 -0600

    Handle note off velocities for pads

--- src/app.js
+++ src/app.js
@@ -108,6 +108,7 @@ function handlePadClick(pad, element) {
         pad.midi.channel,
         pad.midi.note,
         pad.midi.onVelocity ?? 100,
+        pad.midi.offVelocity ?? 0,
         nextState
       );
     } else if (pad.midi.type === "cc") {
